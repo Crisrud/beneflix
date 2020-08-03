0dc63965d131f2703dc7ab8673412cedd3954563
CristianoSilva <cristiano.scarlat@hotmail.com>
2020-08-03 09:06:37 -0300

    Setup inicial do projeto

---
+++ src/components/Menu/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Logo from '../../assets/img/Logo.png';
+import './Menu.css'
+import Button from '../Button';
+
+
+function Menu(){
+
+    return(
+        
+        <nav className="Menu">
+            <a href="/">
+                <img className="Logo" src={Logo} alt="Alura Flix Logo" />
+            </a>
+
+            <Button as="a" href="/">
+                Novo vídeo
+            </Button>
+        </nav>
+    );
+}
+
+export default Menu;
